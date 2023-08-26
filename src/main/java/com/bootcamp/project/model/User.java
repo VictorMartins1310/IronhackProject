10f16de30f1b87c84252a0453e8ea0c8eb800139
VictorMartins1310 <Victor.Martins.1310@hotmail.com>
2023-08-26 02:01:08 +0200

    Added Roles

--- src/main/java/com/bootcamp/project/model/User.java
+++ src/main/java/com/bootcamp/project/model/User.java
@@ -5,7 +5,11 @@ import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.UUID;
+
+import static jakarta.persistence.FetchType.EAGER;
 
 @Entity
 @Data
@@ -21,10 +25,13 @@ public class User {
     private String lastName;
     private String birthDate;
 
+    @ManyToMany(fetch = EAGER)
+    private Collection<Role> roles = new ArrayList<>();
     public User(String email, String password) {
         this.email = email;
         this.password = password;
     }
+
     public void updateDetails(String email, String firstName, String lastName, String birthDate){
         setEmail(email);
         setFirstName(firstName);
