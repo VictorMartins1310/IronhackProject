f4c3c53b7b48337b0d29f53a61f0d2a01365c0ae
VictorMartins <Victor.Martins.1310@hotmail.com>
2024-04-10 02:04:07 +0100

    Adjustments

--- src/main/java/com/bootcamp/project/model/User.java
+++ src/main/java/com/bootcamp/project/model/User.java
@@ -28,11 +28,15 @@ public class User {
     @DateTimeFormat(pattern = "yyyy-mm-dd")
     private LocalDate birthDate;
 
-    private Role role;
+    @ManyToMany(fetch = EAGER)
+    private Collection<Role> roles = new ArrayList<>();
 
     public User(String email, String password) {
         this.email = email;
         this.password = password;
+    }
+    public void addRole(Role role){
+        roles.add(role);
     }
 
     public void updateDetails( String firstName, String lastName, LocalDate birthDate) {
