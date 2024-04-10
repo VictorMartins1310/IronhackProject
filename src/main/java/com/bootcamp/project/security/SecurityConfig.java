e82442edc388e9f0fe4d938a9897f3e4450e57a3
VictorMartins <Victor.Martins.1310@hotmail.com>
2024-04-10 01:57:34 +0100

    Adjustments

--- src/main/java/com/bootcamp/project/security/SecurityConfig.java
+++ src/main/java/com/bootcamp/project/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.bootcamp.project.security;
 
 import com.bootcamp.project.security.filters.CustomAuthenticationFilter;
 import com.bootcamp.project.security.filters.CustomAuthorizationFilter;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
